d069696defcabd516206458e247a7ddcca24fd3d
Uthesh Ganapathy E <120074750+Uthesh56@users.noreply.github.com>
2023-09-12 22:30:56 +0530

    Update FRS_CODE.py

--- FRS_CODE.py
+++ FRS_CODE.py
@@ -1,5 +1,4 @@
 # IMPORTING ESSENTIAL PACKAGES FOR FACE MASK DETECTION
-
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
